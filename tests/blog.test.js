0a7feaacc4ee9b2dcb7530b5848ed965f4ec2ad0
Gabriel Grover <gabrielgrover@Gabriels-iMac.local>
2019-05-15 22:47:36 -0700

    update test

--- tests/blog.test.js
+++ tests/blog.test.js
@@ -11,10 +11,34 @@ afterEach(async () => {
   await page.close();
 });
 
-test("when logged in can see blog create form", async () => {
-  await page.login();
-  await page.click("a.btn-floating");
-  const label = await page.getContentsOf("form label");
+describe("While logged in", () => {
+  beforeEach(async () => {
+    await page.login();
+    await page.click("a.btn-floating");
+  });
 
-  return expect(label).toEqual("Blog Title");
+  it("can see blog create form", async () => {
+    const label = await page.getContentsOf("form label");
+
+    return expect(label).toEqual("Blog Title");
+  });
+
+  describe("and entering valid inputs into form", () => {
+    
+  });
+
+  describe("And entering invalid inputs into form", () => {
+
+    beforeEach(async () => {
+      await page.click("form button")
+    });
+
+    it("shows an error message", async () => {
+      const titleError = await page.getContentsOf(".title .red-text");
+      const contentError = await page.getContentsOf(".content .red-text");
+
+      expect(titleError).toEqual("You must provide a value");
+      return expect(contentError).toEqual("You must provide a value");
+    });
+  });
 });
