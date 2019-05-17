4a1866947c3bfdb4395c0c4090ad2083bc4d9cf4
Gabriel Grover <gabrielgrover@MDR-MAC-GABRIELG.local>
2019-05-17 11:17:26 -0700

    implement travis ci configuration

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://data-kid:Fs1123581321!@ds255265.mlab.com:55265/advanced_node_course',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379'
 };
